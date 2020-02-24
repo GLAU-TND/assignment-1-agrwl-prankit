def08eab75b21bc450ac11a2c295ec4cc42556bb
Abhishek15725 <Abhishek15725@gmail.com>
2020-02-24 22:43:41 +0530

    initial commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -15,7 +15,7 @@ public class Main {
         boolean flag = true;
         while (flag == true) {
             System.out.println();
-            System.out.println("welcome to Abhi's contact list");
+            System.out.println("welcome to Prankit's contact list");
             System.out.println("Press 1 to add a new contact\n" +
                     "Press 2 to view all contacts\n" +
                     "Press 3 to search for a contact\n" +
